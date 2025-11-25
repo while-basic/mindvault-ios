a28b092268d1e910963820c96e8ef0dbe98a4166
while-basic <c_celaya@icloud.com>
2025-11-24 17:52:58 -0700

    Initial Commit

---
+++ mindvault/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mindvault
+//
+//  Created by Christopher Celayac on 11/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mindvault/mindvaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  mindvaultApp.swift
+//  mindvault
+//
+//  Created by Christopher Celayac on 11/24/25.
+//
+
+import SwiftUI
+
+@main
+struct mindvaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
